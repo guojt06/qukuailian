9d94e496dedf85118658f23b9cb03a2851efd58d
郭小宝 <8884067+guo-xiaobao@user.noreply.gitee.com>
2025-12-15 09:58:23 +0800

    任务二完成

--- qukuailian/go/go基础/任务二/Channel.go
+++ qukuailian/go/go基础/任务二/Channel.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -188,9 +189,80 @@ func getchannel(chi <-chan int, wg *sync.WaitGroup) {
 
 // 题目 ：实现一个带有缓冲的通道，生产者协程向通道中发送100个整数，消费者协程从通道中接收这些整数并打印。
 // 考察点 ：通道的缓冲机制。
+func setchannelbuffer(ch chan<- int, wg *sync.WaitGroup) {
+	for i := 0; i < 100; i++ {
+		ch <- i
+		fmt.Printf("生产: %d\n", i)
+	}
+	close(ch) // 关闭通道
+	wg.Done()
+}
+
+func getchannelbuffer(chi <-chan int, wg *sync.WaitGroup) {
+	for v := range chi {
+		fmt.Printf("接收: %d\n", v)
+	}
+	wg.Done()
+}
+
+// 锁机制
+// 题目 ：编写一个程序，使用 sync.Mutex 来保护一个共享的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+// 考察点 ： sync.Mutex 的使用、并发数据安全。
+// 题目 ：使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+// 考察点 ：原子操作、并发数据安全。
+
+func synchronize(counter int, mutex *sync.Mutex, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				//加锁
+				mutex.Lock()
+				counter++
+				//解锁
+				mutex.Unlock()
+
+			}
+			// 等待所有goroutine完成
+			fmt.Printf("计数器的值为：%d\n", counter)
+		}()
+		wg.Wait()
+	}
+	// 等待所有goroutine完成
+	fmt.Printf("计数器的值为：%d\n", counter)
+}
+
+func synchronize1(counter int64, mutex *sync.Mutex, wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				atomic.AddInt64(&counter, 1)
+
+			}
+			// 等待所有goroutine完成
+			fmt.Printf("计数器的值为：%d\n", counter)
+		}()
+		wg.Wait()
+	}
+	// 等待所有goroutine完成
+	fmt.Printf("计数器的值为：%d\n", counter)
+}
+
+// // 题目 ：编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值。
+// 考察点 ：指针的使用、值传递与引用传递的区别。
+// 题目 ：实现一个函数，接收一个整数切片的指针，将切片中的每个元素乘以2。
+// 考察点 ：指针运算、切片操作。
+func increase1(num *int) {
+	*num += 10
+}
+
+func double1(num *[]int) {}
 
 func main() {
-	// num := 15
+	num := 15
 	// increase(&num)
 	// fmt.Printf("修改后的值为：%d\n", num)
 
@@ -233,16 +305,32 @@ func main() {
 
 	// emp.printInfo()
 
-	//创建管道
-	a := make(chan int)
-	//创建计数器
-	var wg sync.WaitGroup
-	wg.Add(2)
-	//启动生产者
-	go setchannel(a, &wg)
-	//启动消费者
-	go getchannel(a, &wg)
-	//等待执行完成
-	wg.Wait()
-	fmt.Println("完成")
+	// //创建管道
+	// a := make(chan int)
+	// //创建计数器
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+	// //启动生产者
+	// go setchannel(a, &wg)
+	// //启动消费者
+	// go getchannel(a, &wg)
+	// //等待执行完成
+	// wg.Wait()
+	// fmt.Println("完成")
+
+	//创建一个缓存管道
+	// wr := make(chan int, 10)
+	// //创建计数器
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+	// go setchannelbuffer(wr, &wg)
+	// go getchannelbuffer(wr, &wg)
+	// wg.Wait()
+	// fmt.Println("完成")
+
+	// synchronize(0, &sync.Mutex{}, &sync.WaitGroup{})
+	// synchronize1(0, &sync.Mutex{}, &sync.WaitGroup{})
+
+	increase1(&num)
+	fmt.Printf("计数器的值为：%d\n", num)
 }
